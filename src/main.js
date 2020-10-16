09c13449ea07cfc7d78d561ac4df00dfba9f8892
Chris Bradley <chris_bradley@sweetwater.com>
2020-10-15 22:44:34 -0400

    add uvue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import createRouter from './router'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+export default () => {
+  const router = createRouter()
+  return new Vue({
+    router,
+    render: h => h(App)
+  })
+}
